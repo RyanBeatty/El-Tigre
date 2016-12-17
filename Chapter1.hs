c7d3c84a775c449e2fe0c1c8eb0c684218ca158f
RyanBeatty <rvbeatty@email.wm.edu>
2016-12-16 19:46:07 -0800

    added documentation.

--- Chapter1.hs
+++ Chapter1.hs
@@ -1,6 +1,11 @@
+-- Solution for the programming exercise at the end of chapter 1.
+
 import Data.List (find)
 import Control.Applicative 
 
+-----------------------------------------------------
+-- The following types implement the grammar for the 
+-- straight line language described in Ch 1.
 type Id = String
 
 data BinOp = Plus | Minus | Times | Div
@@ -13,7 +18,9 @@ data Exp = IdExp Id
          | NumExp Int
          | OpExp Exp BinOp Exp
          | EseqExp Stm Exp
+-----------------------------------------------------
 
+-- Example prog to use for testing
 prog = CompoundStm (AssignStm "a" (OpExp (NumExp 5) Plus (NumExp 3))) (CompoundStm (AssignStm "b" (EseqExp (PrintStm [IdExp "a", OpExp (IdExp "a") Minus (NumExp 1)]) (OpExp (NumExp 10) Times (IdExp "a")))) (PrintStm [IdExp "b"]))
 
 -- Returns the maximum number of args of any PrintStm of any Stm.
@@ -28,15 +35,18 @@ maxArgsExp (EseqExp s e)   = max (maxArgsStm s) (maxArgsExp e)
 maxArgsExp (OpExp e1 _ e2) = max (maxArgsExp e1) (maxArgsExp e2)
 maxArgsExp _               = 0
 
-
+-- Symbol table. Probably could've used Map in the Prelude
 type Table = [(Id, Int)]
 
+-- Update the Table
 update :: Table -> Id -> Int -> Table
 update table name value = (name, value) : table
 
+-- Lookup Key in Table
 lookup' :: Table -> Id -> Maybe Int
 lookup' table name = snd <$> find (\(a, b) -> a == name) table 
 
+-- Interpret a Statement.
 interpStm :: Stm -> Table -> IO (Table)
 interpStm (AssignStm name exp) t =
   do (val, t') <- interpExp exp t
@@ -46,6 +56,8 @@ interpStm (AssignStm name exp) t =
 interpStm (CompoundStm s1 s2) t = interpStm s1 t >>= interpStm s2
 interpStm (PrintStm es) t = printStm es t
 
+-- Helper to Interpret a PrintStm. Probably could
+-- rewrite this to be cleaner.
 printStm :: [Exp] -> Table -> IO (Table)
 printStm [] t = putStr "\n" >> return t
 printStm (e:es) t =
@@ -53,6 +65,7 @@ printStm (e:es) t =
      putStr $ show v ++ " "
      printStm es t'
 
+-- Interpret an Exp.
 interpExp :: Exp -> Table -> IO (Maybe Int, Table)
 interpExp (IdExp name) t     = return (lookup' t name, t)
 interpExp (NumExp num) t     = return (Just num, t)
@@ -61,12 +74,14 @@ interpExp (OpExp e1 op e2) t = do (r1, t')  <- interpExp e1 t
                                   return (binOp op <$> r1 <*> r2, t'')
 interpExp (EseqExp s e) t    = interpStm s t >>= interpExp e
 
+-- Map a BinOp to the actual operation function.
 binOp :: BinOp -> (Int -> Int -> Int)
 binOp Plus  = (+)
 binOp Minus = (-)
 binOp Times = (*)
 binOp Div   = (div)
 
+-- Interpret a Program from the Straight Line Language.
 interp :: Stm -> IO ()
 interp stm =
   do table <- interpStm stm []
--- Exercises1.hs
+++ Exercises1.hs
@@ -1,8 +1,12 @@
+-- Solutions for the exercises at the end of Chapter 1.
 
 type Key = String
 
+-- Tree data structure where every node contains a key
+-- and a value associated with that key.
 data Tree k v = Leaf | Node (Tree k v) k v (Tree k v)
 
+-- Return True if Key is in the tree and false otherwise.
 member :: Tree Key b -> Key -> Bool
 member Leaf _ = False
 member (Node l k v r) key
@@ -10,6 +14,9 @@ member (Node l k v r) key
   | key < k   = member l key
   | otherwise = member r key 
 
+-- Insert a Key with a value in the tree if Key is not
+-- in the tree, or update the already existing key with
+-- the new value.
 insert :: Tree Key a -> Key -> a -> Tree Key a
 insert Leaf k v = Node Leaf k v Leaf
 insert (Node l k v r) key val
@@ -17,6 +24,7 @@ insert (Node l k v r) key val
   | key < k   = Node (insert l key val) k v r
   | otherwise = Node l k v (insert r key val)
 
+-- Return the value associated with the key or nothing.
 lookup' :: Tree Key a -> Key -> Maybe a
 lookup' Leaf _ = Nothing
 lookup' (Node l k v r) key
