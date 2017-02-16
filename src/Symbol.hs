c5277bc081c02e06bf869f19d032f31bd077e7fb
RyanBeatty <rvbeatty@email.wm.edu>
2017-02-15 20:42:06 -0800

    Done implementing symbol table. Added documentation as well. Need to fix export.

--- src/Symbol.hs
+++ src/Symbol.hs
@@ -1,5 +1,6 @@
 module Symbol () where
 
+import Control.Applicative
 import Control.Monad.State
 import qualified Data.COrdering as COrd
 -- TODO: See if I can use a faster hash map library.
@@ -49,24 +50,41 @@ symbol name = do
             put $ makeSymMap t' (succ sym')
             return sym'
 
+-- An STable is an avl tree that contains Symbol to binding mappings.
 type STable a = AVL.AVL (Symbol, a)
 
+-- A SymTable has a SymMap for mapping identifiers to Symbols and a
+-- STable mapping Symbols to bindings.
 data SymTable a = SymTable {
       symMap :: SymMap
     , stable :: STable a
 } deriving (Show)
 
-symComp :: (Symbol, a) -> (Symbol, a) -> COrd.COrdering (Symbol, a)
-symComp s = COrd.fstByCC (\(s1, _) (s2, _) -> compare s1 s2) s
+-- Combining comparison function that takes two STable elements and
+-- returns the first element if both of the symbol parts are equal.
+elemComp :: (Symbol, a) -> (Symbol, a) -> COrd.COrdering (Symbol, a)
+elemComp s = COrd.fstByCC (\(s1, _) (s2, _) -> compare s1 s2) s
 
+-- Combining comparison function that takes a symbol and an STable
+-- element and returns the STable element if both symbol parts
+-- are equal.
+symComp :: Symbol -> (Symbol, a) -> COrd.COrdering (Symbol, a)
+symComp s el = COrd.sndByCC (\s1 (s2, _) -> compare s1 s2) s el
+
+-- Make a SymTable.
 makeSymTable :: SymMap -> STable a -> SymTable a
 makeSymTable m t = SymTable { symMap = m, stable = t }
 
+-- Make an empty SymTable.
 empty :: SymTable a
 empty = makeSymTable newSymMap AVL.empty
 
-enter :: Symbol -> a -> SymTable a -> SymTable a
-enter sym binding t = makeSymTable m (AVL.push (symComp element) element (stable t))
+-- Return the binding of a symbol if it exists.
+look :: SymTable a -> Symbol -> Maybe a
+look t sym = snd <$> AVL.tryRead (stable t) (symComp sym)
+
+-- Create a new binding with the given symbol.
+enter :: (Symbol, a) -> SymTable a -> SymTable a
+enter element t = makeSymTable m (AVL.push (elemComp element) element (stable t))
     where m       = symMap t
           st      = stable t
-          element = (sym, binding)
