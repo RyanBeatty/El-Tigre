3b586cabb03e388cb6d1befcbc2d27dd76dd2e6b
RyanBeatty <rvbeatty@email.wm.edu>
2017-02-28 20:23:44 -0800

    added type checking for let expressions.

--- src/Semant.hs
+++ src/Semant.hs
@@ -17,13 +17,47 @@ makeExpType e t = ExpType { expr = e, ty = t }
 checkInt :: ExpType -> Bool
 checkInt expty = ty expty == Types.INT
 
+--transVar :: Env.VEnv -> Env.TEnv -> AST.Var -> ExpType
+--transVar = undefined
+transDec :: Env.VEnv -> Env.TEnv -> AST.Dec -> (Env.VEnv, Env.TEnv)
+transDec = undefined
+transTy  :: Env.TEnv -> AST.Type -> Types.Type
+transTy = undefined
+
+-- Translate a list of declrations and modify the var env and type env accordingly.
+transDecs :: Env.VEnv -> Env.TEnv -> [AST.Dec] -> (Env.VEnv, Env.TEnv)
+transDecs venv tenv []     = (venv, tenv)
+transDecs venv tenv [x]    = transDec venv tenv x
+transDecs venv tenv (x:xs) =
+    let (venv', tenv') = transDec venv tenv x
+    in transDecs venv' tenv' xs 
+
+-- This is really only supposed to be used to translate a Seq expression
+-- and the list of the expressions in the body of the Let expression.
+-- NOTE: This function assumes that the list of expressions is not empty.
+-- TODO: This is sort of a hack because I didn't let the body of a 
+-- Let expression be an Exp so that Seq could be used. Figure out a more
+-- elegant solution than this.
+transSeq :: Env.VEnv -> Env.TEnv -> [AST.Exp] -> ExpType
+transSeq venv tenv [x]    = transExp venv tenv x
+transSeq venv tenv (_:xs) = transSeq venv tenv xs  
+
 transExp :: Env.VEnv -> Env.TEnv -> AST.Exp -> ExpType
 transExp venv tenv (AST.IntLit _)    = makeExpType () Types.INT
 transExp venv tenv (AST.StringLit _) = makeExpType () Types.STRING
 transExp venv tenv (AST.Neg _)       = makeExpType () Types.INT
 transExp venv tenv (AST.ArithOp _ left right)
     | checkInt (transExp venv tenv left) && checkInt (transExp venv tenv right) = makeExpType () Types.INT
-    | otherwise = error "need two ints"
+    | otherwise = error "arithmetic operators need two ints"
+transExp venv tenv (AST.CompOp _ left right)
+    | checkInt (transExp venv tenv left) && checkInt (transExp venv tenv right) = makeExpType () Types.INT
+    | otherwise = error "comparison operators require two ints"
+transExp venv tenv (AST.LogOp _ left right)
+    | checkInt (transExp venv tenv left) && checkInt (transExp venv tenv right) = makeExpType () Types.INT
+    | otherwise = error "comparison operators require two ints"
+transExp venv tenv (AST.Let decs body) =
+    let (venv', tenv') = transDecs venv tenv decs
+    in transSeq venv' tenv' body
 
 testTrans :: String -> IO ()
 testTrans input = 
