cb7b83dd9677ee89eadc4add8788a92d4c2609ed
RyanBeatty <rvbeatty@email.wm.edu>
2017-03-08 19:19:04 -0800

    Wrote some more test code and test helper functions.

--- test/SemantTest.hs
+++ test/SemantTest.hs
@@ -8,10 +8,35 @@ import Semant
 import qualified Types as T
 
 semantTests :: TestTree
-semantTests = testGroup "Semant.hs Tests" [test1, testIntLit]
+semantTests = testGroup "Semant.hs Tests" [
+      testIntLit
+    , testStringLit
+    , testNeg
+    , testArithOp]
 
-test1 :: TestTree
-test1 = testCase "1 yields INT" $ True `compare` False @?= GT
+getType :: String -> Either String T.Type
+getType s = ty <$> transProg s
+
+assertTrue :: (Eq a) => a -> a -> Assertion
+assertTrue x y = x == y @?= True
+
+yieldsType :: String -> T.Type -> Assertion
+yieldsType s t = assertTrue (getType s) (return t)
+
+yieldsInt :: String -> Assertion
+yieldsInt s = yieldsType s T.INT
+
+yieldsString :: String -> Assertion
+yieldsString s = yieldsType s T.STRING
 
 testIntLit :: TestTree
-testIntLit = testCase "1 yields INT" $ (ty <$> transProg "1") == (return T.INT) @?= True
+testIntLit = testCase "IntLit: 1 yields INT" $ yieldsInt "1"
+
+testStringLit :: TestTree
+testStringLit = testCase "StringLit: \"hello\" yields STRING" $ yieldsString "\"hello\""
+
+testNeg :: TestTree
+testNeg = testCase "Neg: -1 yields INT" $ yieldsInt "-1"
+
+testArithOp :: TestTree
+testArithOp = testCase "ArithOp: 1 + 1 yields INT" $ yieldsInt "1 + 1"
