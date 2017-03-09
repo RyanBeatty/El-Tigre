30d3bf5407b726f313f3f98ee6b5bfcd626e1d1b
RyanBeatty <rvbeatty@email.wm.edu>
2017-03-09 07:27:51 -0800

    Added compop tests for ints.

--- test/SemantTest.hs
+++ test/SemantTest.hs
@@ -16,7 +16,13 @@ semantTests = testGroup "Semant.hs Tests" [
     , testArithOpMinus
     , testArithOpMult
     , testArithOpDiv
-    , testArithOpMismatch]
+    , testArithOpMismatch
+    , testCompOpGT
+    , testCompOpLT
+    , testCompOpGE
+    , testCompOpLE
+    , testCompOpEQ
+    , testCompOpNEQ]
 
 getType :: String -> Either String T.Type
 getType s = ty <$> transProg s
@@ -57,3 +63,21 @@ testArithOpDiv = testCase "ArithOp Div: 1 / 1 yields INT" $ yieldsInt "1 / 1"
 testArithOpMismatch :: TestTree
 testArithOpMismatch = testCase "ArithOp Mismatch: 1 + \"hello\" yields type error" $
     (getType "1 + \"hello\"") == Left "Arithmetic operators need two ints. Got <int> and <string>" @?= False
+
+testCompOpGT :: TestTree
+testCompOpGT = testCase "CompOp GT: 1 > 1 yields INT" $ yieldsInt "1 > 1"
+
+testCompOpLT :: TestTree
+testCompOpLT = testCase "CompOp LT: 1 < 1 yields INT" $ yieldsInt "1 < 1"
+
+testCompOpGE :: TestTree
+testCompOpGE = testCase "CompOp GE: 1 >= 1 yields INT" $ yieldsInt "1 >= 1"
+
+testCompOpLE :: TestTree
+testCompOpLE = testCase "CompOp LE: 1 <= 1 yields INT" $ yieldsInt "1 <= 1"
+
+testCompOpEQ :: TestTree
+testCompOpEQ = testCase "CompOp EQ: 1 = 1 yields INT" $ yieldsInt "1 = 1"
+
+testCompOpNEQ :: TestTree
+testCompOpNEQ = testCase "CompOp NEQ: 1 <> 1 yields INT" $ yieldsInt "1 <> 1"
