6fc7ee26c62c81f49e302654da51a8f50bc3b938
RyanBeatty <rvbeatty@email.wm.edu>
2017-03-09 07:59:31 -0800

    removed lengthy test descriptions.

--- test/SemantTest.hs
+++ test/SemantTest.hs
@@ -36,49 +36,49 @@ yieldsString :: String -> Assertion
 yieldsString s = yieldsType s T.STRING
 
 testIntLit :: TestTree
-testIntLit = testCase "IntLit: 1 yields INT" $ yieldsInt "1"
+testIntLit = testCase "IntLit" $ yieldsInt "1"
 
 testStringLit :: TestTree
-testStringLit = testCase "StringLit: \"hello\" yields STRING" $ yieldsString "\"hello\""
+testStringLit = testCase "StringLit" $ yieldsString "\"hello\""
 
 testNeg :: TestTree
-testNeg = testCase "Neg: -1 yields INT" $ yieldsInt "-1"
+testNeg = testCase "Neg" $ yieldsInt "-1"
 
 testArithOp :: TestTree
 testArithOp = testGroup "ArithOp Tests"
-  [ testCase "ArithOp Plus: 1 + 1 yields INT" $ yieldsInt "1 + 1"
-  , testCase "ArithOp Minus: 1 - 1 yields INT" $ yieldsInt "1 - 1"
-  , testCase "ArithOp Mult: 1 * 1 yields INT" $ yieldsInt "1 + 1"
-  , testCase "ArithOp Mult: 1 * 1 yields INT" $ yieldsInt "1 + 1"
-  , testCase "ArithOp Div: 1 / 1 yields INT" $ yieldsInt "1 / 1"
-  , testCase "ArithOp Mismatch: 1 + \"hello\" yields type error" $
+  [ testCase "ArithOp Plus" $ yieldsInt "1 + 1"
+  , testCase "ArithOp Minus" $ yieldsInt "1 - 1"
+  , testCase "ArithOp Mult" $ yieldsInt "1 + 1"
+  , testCase "ArithOp Mult" $ yieldsInt "1 + 1"
+  , testCase "ArithOp Div" $ yieldsInt "1 / 1"
+  , testCase "ArithOp Mismatch" $
         yieldsTypeError "1 + \"hello\"" "Arithmetic operators need two ints. Got <INT> and <STRING>"
   ]
 
 testCompOp :: TestTree
 testCompOp = testGroup "CompOp Tests"
-  [ testCase "CompOp GT: 1 > 1 yields INT" $ yieldsInt "1 > 1"
-  , testCase "CompOp LT: 1 < 1 yields INT" $ yieldsInt "1 < 1"
-  , testCase "CompOp GE: 1 >= 1 yields INT" $ yieldsInt "1 >= 1"
-  , testCase "CompOp LE: 1 <= 1 yields INT" $ yieldsInt "1 <= 1"
-  , testCase "CompOp EQ: 1 = 1 yields INT" $ yieldsInt "1 = 1"
-  , testCase "CompOp NEQ: 1 <> 1 yields INT" $ yieldsInt "1 <> 1"
-  , testCase "CompOp Mismatch: 1 > \"hello\" yields type error" $
+  [ testCase "CompOp GT" $ yieldsInt "1 > 1"
+  , testCase "CompOp LT" $ yieldsInt "1 < 1"
+  , testCase "CompOp GE" $ yieldsInt "1 >= 1"
+  , testCase "CompOp LE" $ yieldsInt "1 <= 1"
+  , testCase "CompOp EQ" $ yieldsInt "1 = 1"
+  , testCase "CompOp NEQ" $ yieldsInt "1 <> 1"
+  , testCase "CompOp Mismatch" $
         yieldsTypeError "1 > \"hello\"" "Comparison operators require two ints. Got <INT> and <STRING>"
 
   ]
 
 testLogOp :: TestTree
 testLogOp = testGroup "LogOp Tests"
-  [ testCase "LogOp And: 1 & 1 yields INT" $ yieldsInt "1 & 1"
-  , testCase "LogOp Or: 1 | 1 yields INT" $ yieldsInt "1 | 1"
+  [ testCase "LogOp And" $ yieldsInt "1 & 1"
+  , testCase "LogOp Or" $ yieldsInt "1 | 1"
   ]
 
 testLet :: TestTree
 testLet = testGroup "Let Tests"
-  [ testCase "Let Empty Decs: let in 1 end yields INT" $ yieldsInt "let in 1 end"
-  , testCase "Let Many Exps: let in 1; \"hello\" end yields STRING" $ yieldsString "let in 1; \"hello\" end"
-  , testCase "Let UnTyped VarDec: let var foo := 1 in foo end yields INT" $ yieldsInt "let var foo := 1 in foo end"
-  , testCase "Let Typed VarDec: let var foo : int := 1 in foo end yields INT" $ yieldsInt "let var foo : int := 1 in foo end"
+  [ testCase "Let Empty Decs" $ yieldsInt "let in 1 end"
+  , testCase "Let Many Exps" $ yieldsString "let in 1; \"hello\" end"
+  , testCase "Let UnTyped VarDec" $ yieldsInt "let var foo := 1 in foo end"
+  , testCase "Let Typed VarDec" $ yieldsInt "let var foo : int := 1 in foo end"
   , testCase "Let Typed VarDec Mismatch" $ yieldsTypeError "let var foo : string := 1 in foo end" "Variable Type Mismatch. Declared <STRING> Got <INT>"
   ]
