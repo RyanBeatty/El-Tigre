2f003e1d84c25ea58eafc2d80ca93b2cc51b8f89
RyanBeatty <rvbeatty@email.wm.edu>
2017-03-09 16:44:41 -0800

    small documentation cleanup.

--- test/SemantTest.hs
+++ test/SemantTest.hs
@@ -46,41 +46,41 @@ testNeg = testCase "Neg" $ yieldsInt "-1"
 
 testArithOp :: TestTree
 testArithOp = testGroup "ArithOp Tests"
-  [ testCase "ArithOp Plus" $ yieldsInt "1 + 1"
-  , testCase "ArithOp Minus" $ yieldsInt "1 - 1"
-  , testCase "ArithOp Mult" $ yieldsInt "1 + 1"
-  , testCase "ArithOp Mult" $ yieldsInt "1 + 1"
-  , testCase "ArithOp Div" $ yieldsInt "1 / 1"
-  , testCase "ArithOp Mismatch" $
+  [ testCase "ArithOp: Plus" $ yieldsInt "1 + 1"
+  , testCase "ArithOp: Minus" $ yieldsInt "1 - 1"
+  , testCase "ArithOp: Mult" $ yieldsInt "1 + 1"
+  , testCase "ArithOp: Mult" $ yieldsInt "1 + 1"
+  , testCase "ArithOp: Div" $ yieldsInt "1 / 1"
+  , testCase "ArithOp: Mismatch" $
         yieldsTypeError "1 + \"hello\"" "Arithmetic operators need two ints. Got <INT> and <STRING>"
   ]
 
 testCompOp :: TestTree
 testCompOp = testGroup "CompOp Tests"
-  [ testCase "CompOp GT" $ yieldsInt "1 > 1"
-  , testCase "CompOp LT" $ yieldsInt "1 < 1"
-  , testCase "CompOp GE" $ yieldsInt "1 >= 1"
-  , testCase "CompOp LE" $ yieldsInt "1 <= 1"
-  , testCase "CompOp EQ" $ yieldsInt "1 = 1"
-  , testCase "CompOp NEQ" $ yieldsInt "1 <> 1"
-  , testCase "CompOp Mismatch" $
+  [ testCase "CompOp: GT" $ yieldsInt "1 > 1"
+  , testCase "CompOp: LT" $ yieldsInt "1 < 1"
+  , testCase "CompOp: GE" $ yieldsInt "1 >= 1"
+  , testCase "CompOp: LE" $ yieldsInt "1 <= 1"
+  , testCase "CompOp: EQ" $ yieldsInt "1 = 1"
+  , testCase "CompOp: NEQ" $ yieldsInt "1 <> 1"
+  , testCase "CompOp: Mismatch" $
         yieldsTypeError "1 > \"hello\"" "Comparison operators require two ints. Got <INT> and <STRING>"
   ]
 
 testLogOp :: TestTree
 testLogOp = testGroup "LogOp Tests"
-  [ testCase "LogOp And" $ yieldsInt "1 & 1"
-  , testCase "LogOp Or" $ yieldsInt "1 | 1"
+  [ testCase "LogOp: And" $ yieldsInt "1 & 1"
+  , testCase "LogOp: Or" $ yieldsInt "1 | 1"
   ]
 
 testLet :: TestTree
 testLet = testGroup "Let Tests"
-  [ testCase "Let Empty Decs" $ yieldsInt "let in 1 end"
-  , testCase "Let Many Exps" $ yieldsString "let in 1; \"hello\" end"
-  , testCase "Let UnTyped VarDec" $ yieldsInt "let var foo := 1 in foo end"
-  , testCase "Let Typed VarDec" $ yieldsInt "let var foo : int := 1 in foo end"
-  , testCase "Let Typed VarDec Mismatch" $
+  [ testCase "Let: Empty Decs" $ yieldsInt "let in 1 end"
+  , testCase "Let: Many Exps" $ yieldsString "let in 1; \"hello\" end"
+  , testCase "Let: UnTyped VarDec" $ yieldsInt "let var foo := 1 in foo end"
+  , testCase "Let: Typed VarDec" $ yieldsInt "let var foo : int := 1 in foo end"
+  , testCase "Let: Typed VarDec Mismatch" $
         yieldsTypeError "let var foo : string := 1 in foo end" "Variable Type Mismatch. Declared <STRING> Got <INT>"
-  , testCase "Let Undeclared Typed VarDec" $
+  , testCase "Let: Undeclared Typed VarDec" $
         yieldsTypeError "let var foo : bar := 1 in foo end" "Undeclared Type <bar>"
   ]
