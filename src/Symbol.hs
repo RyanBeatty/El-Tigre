0bc92ad25c93dfb986a2198dd3c48d9d6e8ebe8e
RyanBeatty <rvbeatty@email.wm.edu>
2017-03-13 19:10:09 -0700

    Everything builds. Two tests fail because of emptySymbolMap hack. Need way to make sure Symbols are consistent between environment and lexing.

--- src/Symbol.hs
+++ src/Symbol.hs
@@ -2,6 +2,7 @@
 -- for type checking.
 
 module Symbol (
+    makeSymbol,
     Symbol,
     SymbolMap,
     SymbolTable,
@@ -88,91 +89,3 @@ look = Map.lookup
 -- Map Symbol to the new binding.
 enter :: Symbol -> a -> SymbolTable a -> SymbolTable a
 enter = Map.insert
-
----- Return symbol that the identifier maps to if it exists.
----- If it doesn't, create a new mapping.
---symbol :: Id -> SymState Symbol
---symbol name = do
---    -- Get the current SymMap and try to lookup name.
---    curState <- get
---    let sm = smap curState
---    case Map.lookup name sm of
---        Just sym -> return sym
---        Nothing  -> do
---            -- Make new mapping.
---            let sym' = nextSym curState
---            let sm'   = Map.insert name sym' sm
---            put $ makeSymMap sm' (succ sym')
---            return sym'
-
---lookupSymbol :: Id -> SymState (Maybe Symbol)
---lookupSymbol s = do
---    -- Get the current SymMap and try to lookup name.
---    curState <- get
---    let sm = smap curState
---    return $ Map.lookup s sm
-
----- An STable is an avl tree that contains Symbol to binding mappings.
---type STable a = AVL.AVL (Symbol, a)
-
----- A SymTable has a SymMap for mapping identifiers to Symbols and a
----- STable mapping Symbols to bindings.
---data SymTable a = SymTable {
---      symMap :: SymMap
---    , stable :: STable a
---} deriving (Show)
-
----- Combining comparison function that takes two STable elements and
----- returns the first element if both of the symbol parts are equal.
---elemComp :: (Symbol, a) -> (Symbol, a) -> COrd.COrdering (Symbol, a)
---elemComp s = COrd.fstByCC (\(s1, _) (s2, _) -> compare s1 s2) s
-
----- Combining comparison function that takes a symbol and an STable
----- element and returns the STable element if both symbol parts
----- are equal.
---symComp :: Symbol -> (Symbol, a) -> COrd.COrdering (Symbol, a)
---symComp s el = COrd.sndByCC (\s1 (s2, _) -> compare s1 s2) s el
-
----- Make a SymTable.
---makeSymTable :: SymMap -> STable a -> SymTable a
---makeSymTable m t = SymTable { symMap = m, stable = t }
-
----- Takes a list of pairs with the Id's as the first element and
----- creates a SymTable from the list.
---fromList :: [(Id, a)] -> SymTable a
---fromList xs = makeSymTable sm tree
---    where (xs', sm) = runState action newSymMap
---          -- create a SymState action that will map all of the Id's in the
---          -- input list to Symbols. and return the input list with the Id's
---          -- replaced by the Symbol the Id maps to.
---          action    = mapM (\(n, a) -> symbol n >>= \s -> return (s, a)) xs
---          -- Take the result of action and build the symbol table tree.
---          tree      = AVL.asTree elemComp xs'
-
----- Make an empty SymTable.
---empty :: SymTable a
---empty = makeSymTable newSymMap AVL.empty
-
----- Return the binding of a symbol if it exists.
---look :: SymTable a -> Symbol -> Maybe a
---look t sym = snd <$> AVL.tryRead (stable t) (symComp sym)
-
----- Create a new binding with the given symbol.
---enter :: (Symbol, a) -> SymTable a -> SymTable a
---enter element t = makeSymTable m (AVL.push (elemComp element) element (stable t))
---    where m       = symMap t
---          st      = stable t
-
----- Create a new binding with the given symbol. Or do nothing if the binding already exists.
---enterName :: (Id, a) -> SymTable a -> SymTable a
---enterName (name, binding) t = enter (s, binding) (makeSymTable m' st)
---    where m       = symMap t
---          st      = stable t
---          (s, m') = runState (symbol name) m 
-
----- Return the binding of a name if it exists.
---lookName :: SymTable a -> Id -> Maybe a
---lookName t name = getSymbol t name >>= look t
-
---getSymbol :: SymTable a -> Id -> Maybe Symbol
---getSymbol t name = fst $ runState (lookupSymbol name) (symMap t)
