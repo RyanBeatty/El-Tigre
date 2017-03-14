1514d20b81a73a57138f3ca2df5a8353175eeecc
RyanBeatty <rvbeatty@email.wm.edu>
2017-03-14 07:49:05 -0700

    minor cleanup.

--- src/Env.hs
+++ src/Env.hs
@@ -8,38 +8,38 @@ module Env (
 ) where
 
 import Symbol (fromList, SymbolTable, SymbolMap)
-import Types
+import qualified Types as T
 
-data EnvEntry = VarEntry  { envty :: Types.Type }
-              | FuncEntry { formals :: [Types.Type], result :: Types.Type }
+data EnvEntry = VarEntry  { envty :: T.Type }
+              | FuncEntry { formals :: [T.Type], result :: T.Type }
               deriving (Show)
 
-makeVarEntry :: Types.Type -> EnvEntry
+makeVarEntry :: T.Type -> EnvEntry
 makeVarEntry t = VarEntry { envty = t }
 
 -- Environment for tiger type bindings.
-type TEnv = Symbol.SymbolTable Types.Type
+type TEnv = Symbol.SymbolTable T.Type
 
 -- Environment for variable and function bindings.
 type VEnv = Symbol.SymbolTable EnvEntry
 
 -- The built in types environment for Tiger.
 baseTEnv :: Symbol.SymbolMap -> (TEnv, Symbol.SymbolMap)
-baseTEnv sm = Symbol.fromList [("int", Types.INT), ("string", Types.STRING)] sm
+baseTEnv sm = Symbol.fromList [("int", T.INT), ("string", T.STRING)] sm
 
 -- the built in functions environment for Tiger.
 baseVEnv :: Symbol.SymbolMap -> (VEnv, Symbol.SymbolMap)
 baseVEnv sm = Symbol.fromList [
-      ("print", FuncEntry { formals = [Types.STRING], result = Types.UNIT })
-    , ("flush", FuncEntry { formals = [], result = Types.UNIT })
-    , ("getchar", FuncEntry { formals = [], result = Types.STRING })
-    , ("ord", FuncEntry { formals = [Types.STRING], result = Types.INT })
-    , ("chr", FuncEntry { formals = [Types.INT], result = Types.STRING })
-    , ("size", FuncEntry { formals = [Types.STRING], result = Types.INT })
-    , ("substring", FuncEntry { formals = [Types.STRING, Types.INT, Types.INT], result = Types.STRING })
-    , ("concat", FuncEntry { formals = [Types.STRING, Types.STRING], result = Types.STRING })
-    , ("not", FuncEntry { formals = [Types.INT], result = Types.INT })
-    , ("exit", FuncEntry { formals = [Types.INT], result = Types.UNIT })] sm
+      ("print", FuncEntry { formals = [T.STRING], result = T.UNIT })
+    , ("flush", FuncEntry { formals = [], result = T.UNIT })
+    , ("getchar", FuncEntry { formals = [], result = T.STRING })
+    , ("ord", FuncEntry { formals = [T.STRING], result = T.INT })
+    , ("chr", FuncEntry { formals = [T.INT], result = T.STRING })
+    , ("size", FuncEntry { formals = [T.STRING], result = T.INT })
+    , ("substring", FuncEntry { formals = [T.STRING, T.INT, T.INT], result = T.STRING })
+    , ("concat", FuncEntry { formals = [T.STRING, T.STRING], result = T.STRING })
+    , ("not", FuncEntry { formals = [T.INT], result = T.INT })
+    , ("exit", FuncEntry { formals = [T.INT], result = T.UNIT })] sm
 
 buildBaseEnvs :: Symbol.SymbolMap -> (Env.VEnv, Env.TEnv)
 buildBaseEnvs sm = let (venv, sm')  = baseVEnv sm
